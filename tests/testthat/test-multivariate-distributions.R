844669def537b575979b45ce65719d470c8a3a34
Tymoteusz Wołodźko <twolodzko@gmail.com>
2016-06-14 23:02:21 +0200

    ver 1.7.1

--- tests/testthat/test-multivariate-distributions.R
+++ tests/testthat/test-multivariate-distributions.R
@@ -1,3 +1,14 @@
 
 
 # extraDistr::ddirichlet was compared to MCMCpack::ddirichlet
+
+test_that("Testing multivariate distributions", {
+  
+  expect_true(all.equal(rowSums(rmnom(5000, 50, c(2/10, 5/10, 3/10))), rep(50, 5000)))
+  
+  expect_true(all.equal(rowSums(rdirichlet(5000, c(2/10, 5/10, 3/10))), rep(1.0, 5000)))
+  
+  xx <- expand.grid(0:20, 0:20, 0:20)
+  expect_equal(sum(ddirmnom(xx[rowSums(xx) == 20,], 20, c(2/10, 5/10, 3/10))), 1)
+  
+})
