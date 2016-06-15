99dd8ac4a569db85c39039184d8060aef6892fde
Tymoteusz Wołodźko <twolodzko@gmail.com>
2016-06-15 22:59:47 +0200

    ver 1.7.1

--- tests/testthat/test-multivariate-distributions.R
+++ tests/testthat/test-multivariate-distributions.R
@@ -9,6 +9,8 @@ test_that("Testing multivariate distributions", {
   expect_true(all.equal(rowSums(rdirichlet(5000, c(2/10, 5/10, 3/10))), rep(1.0, 5000)))
   
   xx <- expand.grid(0:20, 0:20, 0:20)
-  expect_equal(sum(ddirmnom(xx[rowSums(xx) == 20,], 20, c(2/10, 5/10, 3/10))), 1)
+  expect_equal(sum(ddirmnom(xx[rowSums(xx) == 20,], 20, c(2, 5, 3))), 1)
+  expect_equal(sum(dmnom(xx[rowSums(xx) == 20,], 20, c(2/10, 5/10, 3/10))), 1)
+  expect_equal(sum(dmvhyper(xx[rowSums(xx) == 35,], c(20, 20, 20), 35)), 1)
   
 })
