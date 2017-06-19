741ec2c75719c5d4ac8b3586833ecdc4ff396e6a
Tymoteusz Wołodźko <twolodzko@gmail.com>
2017-06-19 23:42:34 +0200

    more tests for rtnorm

--- tests/testthat/test-rtnorm-in-bounds.R
+++ tests/testthat/test-rtnorm-in-bounds.R
@@ -1,6 +1,6 @@
 
 
-all_in_bounds <- function(object, lower, upper, warning = FALSE) {
+all_in_bounds <- function(object, lower, upper) {
   all(object >= lower & object <= upper)
 }
 
@@ -10,75 +10,60 @@ test_that("Check if values generated using rtnorm stay in bounds", {
   N <- 5000
   mu <- 0
   sigma <- 1
+  
   a <- -1
   b <- 1
+  X <- rtnorm(N, mu, sigma, a, b)
   
-  expect_true(all_in_bounds(rtnorm(N, mu, sigma, a, b), a, b))
-  
-  N <- 5000
-  mu <- 0
-  sigma <- 1
+  expect_true(all_in_bounds(X, a, b))
+
   a <- 0
   b <- 1
+  X <- rtnorm(N, mu, sigma, a, b)
   
-  expect_true(all_in_bounds(rtnorm(N, mu, sigma, a, b), a, b))
-  
-  N <- 5000
-  mu <- 0
-  sigma <- 1
+  expect_true(all_in_bounds(X, a, b))
+
   a <- -1
   b <- 0
+  X <- rtnorm(N, mu, sigma, a, b)
   
-  expect_true(all_in_bounds(rtnorm(N, mu, sigma, a, b), a, b))
-  
-  N <- 5000
-  mu <- 0
-  sigma <- 1
+  expect_true(all_in_bounds(X, a, b))
+
   a <- 4
   b <- 5
+  X <- rtnorm(N, mu, sigma, a, b)
   
-  expect_true(all_in_bounds(rtnorm(N, mu, sigma, a, b), a, b))
-  
-  N <- 5000
-  mu <- 0
-  sigma <- 1
+  expect_true(all_in_bounds(X, a, b))
+
   a <- -5
   b <- -4
+  X <- rtnorm(N, mu, sigma, a, b)
   
-  expect_true(all_in_bounds(rtnorm(N, mu, sigma, a, b), a, b))
-  
-  N <- 5000
-  mu <- 0
-  sigma <- 1
+  expect_true(all_in_bounds(X, a, b))
+
   a <- -Inf
   b <- 5
+  X <- rtnorm(N, mu, sigma, a, b)
   
-  expect_true(all_in_bounds(rtnorm(N, mu, sigma, a, b), a, b))
-  
-  N <- 5000
-  mu <- 0
-  sigma <- 1
+  expect_true(all_in_bounds(X, a, b))
+
   a <- -5
   b <- Inf
+  X <- rtnorm(N, mu, sigma, a, b)
   
-  expect_true(all_in_bounds(rtnorm(N, mu, sigma, a, b), a, b))
-  
-  N <- 5000
-  mu <- 0
-  sigma <- 1
+  expect_true(all_in_bounds(X, a, b))
+
   a <- -Inf
   b <- -4
+  X <- rtnorm(N, mu, sigma, a, b)
   
-  expect_true(all_in_bounds(rtnorm(N, mu, sigma, a, b), a, b))
-  
-  N <- 5000
-  mu <- 0
-  sigma <- 1
+  expect_true(all_in_bounds(X, a, b))
+
   a <- 4
   b <- Inf
+  X <- rtnorm(N, mu, sigma, a, b)
   
-  expect_true(all_in_bounds(rtnorm(N, mu, sigma, a, b), a, b))
-  
-  
+  expect_true(all_in_bounds(X, a, b))
+
 })
 
