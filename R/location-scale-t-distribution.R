6ee4a12c92a96ff8c2b6946d6386d58b90090cb9
Tymoteusz Wolodzko <twolodzko@gmail.com>
2017-09-21 11:14:53 +0200

    document deprecated funs

--- R/location-scale-t-distribution.R
+++ R/location-scale-t-distribution.R
@@ -70,9 +70,32 @@ rlst <- function(n, df, mu = 0, sigma = 1) {
 }
 
 
+
+
+
 # Depreciated since ver 1.8.7
 # To be removed in ver 1.8.8
 
+#' Deprecated functions
+#' 
+#' \emph{Warning}: the \code{dnst}, \code{pnst}, \code{qnst}, and \code{rnst}
+#' functions are now depreciated, please use \code{dlst}, \code{plst}, \code{qlst},
+#' and \code{rlst} functions instead.
+#'
+#' @param x,q	            vector of quantiles.
+#' @param p	              vector of probabilities.
+#' @param n	              number of observations. If \code{length(n) > 1},
+#'                        the length is taken to be the number required.
+#' @param mu              vector of locations
+#' @param sigma           vector of positive valued scale parameters.
+#' @param df              degrees of freedom (> 0, maybe non-integer). \code{df = Inf} is allowed.
+#' @param log,log.p	      logical; if TRUE, probabilities p are given as log(p).
+#' @param lower.tail	    logical; if TRUE (default), probabilities are \eqn{P[X \le x]}
+#'                        otherwise, \eqn{P[X > x]}.
+#' 
+#' @name extraDistr-deprecated
+#' @aliases dnst
+#' 
 #' @export
 
 dnst <- function(x, df, mu = 0, sigma = 1, log = FALSE) {
@@ -80,6 +103,7 @@ dnst <- function(x, df, mu = 0, sigma = 1, log = FALSE) {
   dlst(x, df, mu, sigma, log[1L])
 }
 
+#' @rdname extraDistr-deprecated
 #' @export
 
 pnst <- function(q, df, mu = 0, sigma = 1, lower.tail = TRUE, log.p = FALSE) {
@@ -87,6 +111,7 @@ pnst <- function(q, df, mu = 0, sigma = 1, lower.tail = TRUE, log.p = FALSE) {
   plst(q, df, mu, sigma, lower.tail[1L], log.p[1L])
 }
 
+#' @rdname extraDistr-deprecated
 #' @export
 
 qnst <- function(p, df, mu = 0, sigma = 1, lower.tail = TRUE, log.p = FALSE) {
@@ -94,12 +119,11 @@ qnst <- function(p, df, mu = 0, sigma = 1, lower.tail = TRUE, log.p = FALSE) {
   qlst(p, df, mu, sigma, lower.tail[1L], log.p[1L])
 }
 
+#' @rdname extraDistr-deprecated
 #' @export
 
 rnst <- function(n, df, mu = 0, sigma = 1) {
   .Deprecated("rlst")
   rlst(n, df, mu, sigma)
 }
-
-
 
