43b11ba85bf3f9ce856825d5971767551cc2fa07
Tymoteusz Wołodźko <twolodzko@gmail.com>
2017-11-20 00:26:57 +0100

    dbvpois benchmark

--- tests/testthat/test-pure-r-benchmarks.R
+++ tests/testthat/test-pure-r-benchmarks.R
@@ -25,8 +25,6 @@ dfatigueR <- function(x, alpha, beta, mu) {
          (zb+bz)/(2.0*alpha*z) * dnorm((zb-bz)/alpha))
 }
 
-# dbvpoisR
-
 ddlaplaceR <- function(x, mu, p) {
   ifelse(!is_int(x), 0,
          (1.0-p)/(1.0+p) * p^abs(x-mu))
@@ -149,5 +147,77 @@ test_that("Compare PDF's/PMF's to pure-R benchmarks", {
   expect_equal(dpower(x, 1, 1), dpowerR(x, 1, 1))
   expect_equal(drayleigh(x, 1), drayleighR(x, 1))
   # expect_equal(dsgomp(x, 0.5, 1), dsgompR(x, 0.5, 1)) # ???
+  
+})
+
+
+# ====================================================
+#  Compare dbvpois with code by Karlis and Ntzoufras
+# ====================================================
+
+# Below code was taken from bivpois package ver 0.50-3 (CRAN archive)
+
+pbivpois <- function(x, y=NULL, a, b, c, log=FALSE) {
+  # ------------------------------------------------------------------------------
+  # Karlis and Ntzoufras (2003, 2004)
+  # EM algorithms for Bivariate Poisson Models
+  # ------------------------------------------------------------------------------
+  # x      : matrix or vector of length n
+  # y      : vector of length n. If x is matrix then it is not used
+  # lambda : parameters of the bivariate poisson distribution
+  # log    : argument controlling the calculation of the log-probability or the 
+  #          probability function. 
+  # ------------------------------------------------------------------------------
+  #	
+  
+  lambda <- c(a, b, c) # edited here
+  
+  if ( is.matrix(x) ) {
+    var1<-x[,1]
+    var2<-x[,2]
+  }
+  else if (is.vector(x)&is.vector(y)){
+    if (length(x)==length(y)){
+      var1<-x
+      var2<-y
+    }
+    else{
+      stop('lengths of x and y are not equal')
+    }	
+  }
+  else{
+    stop('x is not a matrix or x and y are not vectors')
+  }
+  n <- length(var1)
+  logbp<-vector(length=n)
+  #
+  for (k in 1:n){
+    x0<-var1[k]
+    y0<-var2[k]
+    xymin<-min( x0,y0 )
+    lambdaratio<-lambda[3]/(lambda[1]*lambda[2])
+    #	
+    i<-0:xymin
+    sums<- -lgamma(var1[k]-i+1)-lgamma(i+1)-lgamma(var2[k]-i+1)+i*log(lambdaratio)
+    maxsums <- max(sums)
+    sums<- sums - maxsums
+    logsummation<- log( sum(exp(sums)) ) + maxsums 
+    logbp[k]<- -sum(lambda) + var1[k] * log( lambda[1] ) + var2[k] * log( lambda[2] ) + logsummation 
+  }
+  if (log) { result<-    logbp }
+  else     { result<-exp(logbp)  }
+  result
+  #	end of function bivpois
+}
+
+
+test_that("Bivariate Poisson distribution agrees with code by Karlis and Ntzoufras", {
+  
+  x <- rbvpois(1000, 7, 8, 5)
+
+  expect_equal(
+    dbvpois(x[,1], x[,2], 7, 8, 5),
+    pbivpois(x[,1], x[,2], 7, 8, 5)
+  )
   
 })
