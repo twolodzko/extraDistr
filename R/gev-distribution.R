ff01faa4741fb528d4ca2e71c2efa065bf5fa006
Tymoteusz Wołodźko <twolodzko@gmail.com>
2017-11-29 22:52:51 +0100

    gev example

--- R/gev-distribution.R
+++ R/gev-distribution.R
@@ -52,6 +52,12 @@
 #'
 #' @examples 
 #' 
+#' curve(dgev(x, xi = -1/2), -4, 4, col = "green", ylab = "")
+#' curve(dgev(x, xi = 0), -4, 4, col = "red", add = TRUE)
+#' curve(dgev(x, xi = 1/2), -4, 4, col = "blue", add = TRUE)
+#' legend("topleft", col = c("green", "red", "blue"), lty = 1,
+#'        legend = expression(xi == -1/2, xi == 0, xi == 1/2), bty = "n")
+#' 
 #' x <- rgev(1e5, 5, 2, .5)
 #' hist(x, 1000, freq = FALSE, xlim = c(0, 50))
 #' curve(dgev(x, 5, 2, .5), 0, 50, col = "red", add = TRUE)
